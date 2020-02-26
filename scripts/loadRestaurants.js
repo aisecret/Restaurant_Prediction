c9f4e0770b573ffff9aa3723b8e6b6c1841626e9
Rusna Tan <rusna@Rusnas-MacBook-Pro.local>
2020-02-26 03:41:40 +0100

    added proposition layout

--- scripts/loadRestaurants.js
+++ scripts/loadRestaurants.js
@@ -1,3 +1,29 @@
+function loadRestaurant(list, id){
+
+  let restaurant = list[id];
+
+  let restaurantNum = restaurant.num;
+  let restaurantType = restaurant.type;
+  let restaurantPrice = restaurant.price;
+  if (restaurantPrice == 0) restaurantPrice = "€";
+  else if (restaurantPrice == 1) restaurantPrice = "€ €";
+      else restaurantPrice = "€ € €";
+  let restaurantDistance = "&#128205: " + restaurant.distance + " km";
+  let restaurantSpiciness = restaurant.spiciness;
+  if (restaurantSpiciness == 0) restaurantSpiciness = "&#x1F336: low";
+  else if (restaurantSpiciness == 1) restaurantSpiciness = "&#x1F336: medium";
+      else restaurantSpiciness = "&#x1F336: high";
+  let restaurantLoudness = restaurant.animation;
+  if (restaurantLoudness == 0) restaurantLoudness = "&#127926: calm";
+  else if (restaurantPrice == 1) restaurantLoudness = "&#127926: moderate";
+      else restaurantLoudness = "&#127926: rowdy";
+  let restaurantSpeed = restaurant.service_speed;
+  if (restaurantSpeed == 0) restaurantSpeed = "&#x1F551: < 10m";
+  else if (restaurantSpeed == 1) restaurantSpeed = "&#x1F551: < 30m";
+      else restaurantSpeed = "&#x1F551: > 30m";
+
+  return [restaurantNum, restaurantType, restaurantDistance, restaurantPrice, restaurantLoudness, restaurantSpeed, restaurantSpiciness];
+}
 
 function loadRestaurants(row, rowItem, name, action, generatingMethod){
     let restaurant_list = getRestaurants();
@@ -9,42 +35,27 @@ function loadRestaurants(row, rowItem, name, action, generatingMethod){
     for (let i = 0; i < row; i++) {
     document.write("<div class='row'>");
     for (let j = 0; j < rowItem; j++) {
-        let restaurant = restaurant_list[i*rowItem + j];
 
-        let restaurantType = restaurant.type;
-
-        let restaurantPrice = restaurant.price;
-        if (restaurantPrice == 0) restaurantPrice = "€";
-        else if (restaurantPrice == 1) restaurantPrice = "€ €";
-            else restaurantPrice = "€ € €";
-        let restaurantDistance = restaurant.distance;
-        let restaurantSpiciness = restaurant.spiciness;
-        if (restaurantSpiciness == 0) restaurantSpiciness = "&#x1F336: low";
-        else if (restaurantSpiciness == 1) restaurantSpiciness = "&#x1F336: medium";
-            else restaurantSpiciness = "&#x1F336: high";
-        let restaurantLoudness = restaurant.animation;
-        if (restaurantLoudness == 0) restaurantLoudness = "&#127926: calm";
-        else if (restaurantPrice == 1) restaurantLoudness = "&#127926: moderate";
-            else restaurantLoudness = "&#127926: rowdy";
-        let restaurantSpeed = restaurant.service_speed;
-        if (restaurantSpeed == 0) restaurantSpeed = "&#x1F551: < 10m";
-        else if (restaurantSpeed == 1) restaurantSpeed = "&#x1F551: < 30m";
-            else restaurantSpeed = "&#x1F551: > 30m";
-
-        document.write("<div class='col-3'>");
+      let restaurantNum = loadRestaurant(restaurant_list, i*rowItem + j)[0];
+      let restaurantType = loadRestaurant(restaurant_list, i*rowItem + j)[1];
+      let restaurantDistance = loadRestaurant(restaurant_list, i*rowItem + j)[2];
+      let restaurantPrice = loadRestaurant(restaurant_list, i*rowItem + j)[3];
+      let restaurantLoudness = loadRestaurant(restaurant_list, i*rowItem + j)[4];              
+      let restaurantSpeed = loadRestaurant(restaurant_list, i*rowItem + j)[5];
+      let restaurantSpiciness = loadRestaurant(restaurant_list, i*rowItem + j)[6];
+      
+        document.write("<div class='col-"+ (12/rowItem) +"'>");
         document.write("<div class='card'>");
-            document.write("<div class='card-horizontal selection' num='"+ restaurant.num +"' id ='"+name+i+j+"' onClick='"+action+"(this)' class='card mb-3' style='opacity:1; border: none; max-width: 540px;'>");
-
-
+        document.write("<div class='card-horizontal selection' num='"+ restaurantNum +"' id ='"+name+i+j+"' onClick='"+action+"(this)' class='card mb-3' style='opacity:1; border: none; max-width: 540px;'>");
 
             document.write("<div class='img-square-wrapper restaurant-rectangle-image' >");
-            document.write("<img class='restaurant-image restImg' src='../images/restaurant_image (" + restaurant.num + ").jpg' width='250' height='170'>");
+            document.write("<img class='restaurant-image restImg' src='../images/restaurant_image (" + restaurantNum + ").jpg' width='250' height='170'>");
             document.write("<img class='restaurant-image filter' src='../images/white.jpg' width='250' height='170'>");
             document.write("</div>");
 
             document.write("<div class='card-body' >");
             document.write("<div class='card-text' style='padding-left: 5px'>"+restaurantType +"</div>");
-            document.write("<div class='card-text' style='padding-left: 5px'>&#128205: "+restaurantDistance +"km</div>");
+            document.write("<div class='card-text' style='padding-left: 5px'>"+restaurantDistance +"</div>");
             document.write("<div class='card-text' style='color:#7CD3A1; font-weight: bold; padding-left: 5px'>"+restaurantPrice +"</div>");
             document.write("<div class='card-text'>"+restaurantLoudness +"</div>");
             document.write("<div class='card-text'>"+restaurantSpeed +"</div>");
@@ -64,7 +75,8 @@ function loadRestaurants(row, rowItem, name, action, generatingMethod){
 
 function select(e){
     const element = document.querySelector("#"+e.id);
-    if (element.style.border=="medium none"){
+
+    if (element.style.border=="none" || element.style.border=="medium none"){
       $(e).css("border", "solid green 3px");
       $(e).css("background-color", "#AFC");
     }else{
@@ -81,19 +93,21 @@ function cross(e){
     $(e).css("opacity", "0.5");*/
 
     const element = document.querySelector("#"+e.id);
-    if (element.style.border=="medium none"){
+    if (element.style.border=="none" || element.style.border=="medium none"){
       $(e).css("border", "solid red 3px");
       $(e).css("background-color", "#FCC");
+      $(e).css("opacity", "0.5");
     }else{
       $(e).css("border", "none");
       $(e).css("background-color", "white");
+      $(e).css("opacity", "1");
     }
 
 }
 
 function acceptProposal(){
     alert('Cool! Enjoy your meal!')
-    window.location.href = 'index.html';
+    window.location.href = 'main.html';
 }
 
 function shuffle(array) {
@@ -116,9 +130,9 @@ function shuffle(array) {
 }
 
 $(document).ready(function(){
-    $("#prediction").hide();
-    $("#showPrediction").click(function(){
-      $("#prediction").show();
+    $("#proposition").hide();
+    $("#showproposition").click(function(){
+      $("#proposition").show();
       $("#selection").hide();
       //console.log($(".selection"))
       $(".selection").each(function() {
